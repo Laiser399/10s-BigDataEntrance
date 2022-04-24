1eb68cb7bda4b33835ed88e5d216fe126fb897af
Sergey Semenov <Laiser399@mail.ru>
2022-04-24 18:59:00 +0300

    PythonScripts: quarters generation

---
+++ CountryMappings/util/4_generate_quarters.py
@@ -0,0 +1,20 @@
+import csv
+
+start_year, start_quarter = 2008, 3
+end_year, end_quarter = 2021, 4
+
+with open('quarters.csv', 'w', newline='') as output_file:
+    writer = csv.writer(output_file, delimiter='\t', escapechar='\\')
+    y, q = start_year, start_quarter
+    while True:
+        if y > end_year:
+            break
+        if y == end_year and q > end_quarter:
+            break
+
+        writer.writerow((y, q))
+
+        q += 1
+        if q == 5:
+            y += 1
+            q = 1
