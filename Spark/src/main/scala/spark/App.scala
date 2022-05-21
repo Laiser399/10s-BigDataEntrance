fbede6222547d69f23f3b479f75df5395cd96eed
Sergey Semenov <Laiser399@mail.ru>
2022-05-21 12:48:54 +0300

    Spark: hello world

--- Spark/src/main/scala/spark/App.scala
+++ Spark/src/main/scala/spark/App.scala
@@ -1,7 +1,39 @@
 package spark
 
+import org.apache.spark.sql.SparkSession
+import org.log4s.getLogger
+
+
 object App {
+  private val logger = getLogger
+  private val appName: String = "MyFirstSparkJobInWholeLife"
+
+  private def master: String = sys.env.getOrElse("SPARK_MASTER", "local[10]")
+
   def main(args: Array[String]): Unit = {
-    println("Hello World!")
+    if (args.length != 2) {
+      logger.error(s"Expected 2 program arguments. Got ${args.length}.")
+      return
+    }
+
+    val inputPath = args(0)
+    val outputPath = args(1)
+
+    val spark = SparkSession
+      .builder()
+      .appName(appName)
+      .master(master)
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val lines = spark
+      .read
+      .textFile(inputPath)
+      .map(x => x + "_somepostfix")
+      .rdd
+
+    lines
+      .saveAsTextFile(outputPath)
   }
 }
