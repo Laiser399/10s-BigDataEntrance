dae5124e2e0b7dd8c410b1ec8c60ff679ba088cf
Sergey Semenov <Laiser399@mail.ru>
2022-05-21 23:31:06 +0300

    Spark: job done... AND LET DEBUG BEGIN

--- Spark/src/main/scala/spark/App.scala
+++ Spark/src/main/scala/spark/App.scala
@@ -1,6 +1,8 @@
 package spark
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{count, lit, quarter, year}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.log4s.getLogger
 
 
@@ -11,29 +13,107 @@ object App {
   private def master: String = sys.env.getOrElse("SPARK_MASTER", "local[10]")
 
   def main(args: Array[String]): Unit = {
-    if (args.length != 2) {
-      logger.error(s"Expected 2 program arguments. Got ${args.length}.")
+    if (args.length != 4) {
+      logger.error(s"Expected 4 program arguments. Got ${args.length}.")
       return
     }
 
-    val inputPath = args(0)
-    val outputPath = args(1)
+    val locationMappingsPath = args(0)
+    val questionsOutputPath = args(1)
+    val answersOutputPath = args(2)
+    val commentsOutputPath = args(3)
 
     val spark = SparkSession
       .builder()
       .appName(appName)
       .master(master)
+      .enableHiveSupport()
       .getOrCreate()
 
     import spark.implicits._
 
-    val lines = spark
+    val posts = spark
       .read
-      .textFile(inputPath)
-      .map(x => x + "_somepostfix")
-      .rdd
+      .table("default.posts")
+      .select(
+        $"Id",
+        $"CreationDate",
+        $"OwnerUserId"
+      )
 
-    lines
-      .saveAsTextFile(outputPath)
+    val questions = posts
+      .where($"PostTypeId" === 1)
+
+    val answers = posts
+      .where($"PostTypeId" === 2)
+
+    val comments = spark
+      .read
+      .table("default.comments")
+      .select(
+        $"Id",
+        $"CreationDate",
+        $"UserId".as("OwnerUserId")
+      )
+
+    val users = spark
+      .read
+      .table("default.users")
+      .select(
+        $"Id",
+        $"Location"
+      )
+
+    val locationMappings = spark
+      .read
+      .option("delimiter", "\t")
+      .option("quote", "\"")
+      .option("escape", "\\")
+      .schema(StructType(Array(
+        StructField("WeirdLocation", StringType, nullable = false),
+        StructField("Country", StringType, nullable = false)
+      )))
+      .csv(locationMappingsPath)
+
+
+    val usersWithCountry = users
+      .join(locationMappings, users("Location") === locationMappings("WeirdLocation"))
+      .select(
+        users("Id"),
+        locationMappings("Country")
+      )
+
+
+    def aggregate(entries: Dataset[Row]): Dataset[Row] = {
+      entries
+        .join(usersWithCountry, entries("OwnerUserId") === usersWithCountry("Id"))
+        .select(
+          usersWithCountry("Country"),
+          year(entries("CreationDate")).as("Year"),
+          quarter(entries("CreationDate")).as("Quarter")
+        )
+        .groupBy(usersWithCountry("Country"), $"Year", $"Quarter")
+        .agg(count(lit(1)))
+    }
+
+    val questionsAggregated = aggregate(questions)
+    val answersAggregated = aggregate(answers)
+    val commentsAggregated = aggregate(comments)
+
+    def saveAggregation(path: String, aggregation: Dataset[Row]): Unit = {
+      aggregation
+        .sort($"Country", $"Year", $"Quarter")
+        .repartition(1)
+        .write
+        .format("csv")
+        .option("delimiter", "\t")
+        .option("quote", "\"")
+        .option("escape", "\\")
+        .save(path)
+    }
+
+    saveAggregation(questionsOutputPath, questionsAggregated)
+    saveAggregation(answersOutputPath, answersAggregated)
+    saveAggregation(commentsOutputPath, commentsAggregated)
   }
 }
